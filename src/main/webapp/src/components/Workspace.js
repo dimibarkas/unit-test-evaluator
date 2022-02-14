f8dcae64b5ab6f99aefc056515dd3eec7e9f59b2
Dimitrios Barkas <53380520+dimibarkas@users.noreply.github.com>
2022-02-14 17:02:08 +0100

    add http request for testing the eval controller

--- src/main/webapp/src/components/Workspace.js
+++ src/main/webapp/src/components/Workspace.js
@@ -1,18 +1,54 @@
 import Editor from "@monaco-editor/react";
-import SubmitButton from "./SubmitButton";
+import {useEffect, useRef, useState} from "react";
+import {Button} from "react-bootstrap";
+import axios from "axios";
 
 const b64string = "aW1wb3J0IG9yZy5qdW5pdC5UZXN0OwoKaW1wb3J0IHN0YXRpYyBvcmcuanVuaXQuQXNzZXJ0Lio7CgpwdWJsaWMgY2xhc3MgSW5zZXJ0aW9uU29ydFRlc3QgewoKICAgIEBUZXN0CiAgICBwdWJsaWMgdm9pZCBzb3J0KCkgewogICAgfQp9";
 
 function Workspace() {
+
+    const editorRef = useRef(null);
+
+    function handleEditorDidMount(editor) {
+        editorRef.current = editor;
+    }
+
+    async function simulateNetworkRequest() {
+        await axios.post("/api/evaluate", {taskId: 1, encodedTestContent: btoa(editorRef.current.getValue())})
+            .then((response) => {
+                console.log(response)
+            })
+    }
+
+    const [isLoading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (isLoading) {
+            simulateNetworkRequest().then(() => {
+                setLoading(false);
+            });
+        }
+    }, [isLoading]);
+
+    const handleClick = () => setLoading(true);
+
+
     return (
         <>
             <div style={{display: "flex", flexDirection: "row-reverse", marginRight: "1rem", marginBottom: "1rem"}}>
-                <SubmitButton />
+                <Button
+                    variant="primary"
+                    disabled={isLoading}
+                    onClick={!isLoading ? handleClick : null}
+                >
+                    {isLoading ? 'Verarbeitung läuft…' : 'Ausführen'}
+                </Button>
             </div>
             <Editor
                 height={"35vh"}
                 defaultLanguage={"java"}
                 theme={"vs-dark"}
+                onMount={handleEditorDidMount}
                 defaultValue={atob(b64string)}
             />
 
