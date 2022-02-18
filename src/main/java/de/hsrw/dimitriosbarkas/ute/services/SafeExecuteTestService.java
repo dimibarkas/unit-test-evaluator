03a4e0cf28152da2cff4e056b358b850fbfdf46f
Dimitrios Barkas <53380520+dimibarkas@users.noreply.github.com>
2022-02-18 12:42:54 +0100

    add SafeExecuteTestService

--- src/main/java/de/hsrw/dimitriosbarkas/ute/services/SafeExecuteTestService.java
+++ src/main/java/de/hsrw/dimitriosbarkas/ute/services/SafeExecuteTestService.java
@@ -1,9 +1,17 @@
 package de.hsrw.dimitriosbarkas.ute.services;
 
+import de.hsrw.dimitriosbarkas.ute.model.Task;
+import de.hsrw.dimitriosbarkas.ute.services.exceptions.CannotConvertToFileException;
+
+import java.io.IOException;
 import java.nio.file.Path;
 
 public interface SafeExecuteTestService {
 
-    Path safelyExecuteTestInTempFolder(String encodedTask, String encodedTest);
+    Path extractFilesToTemplateProject(Task task, String encodedTest) throws CannotConvertToFileException;
+
+    void safelyExecuteTestInTempProject(Path path) throws IOException;
+
+    void generateCoverageReport(Path path) throws IOException;
 
 }
--- src/main/java/de/hsrw/dimitriosbarkas/ute/services/impl/SafeExecuteTestServiceImpl.java
+++ src/main/java/de/hsrw/dimitriosbarkas/ute/services/impl/SafeExecuteTestServiceImpl.java
@@ -1,14 +1,98 @@
 package de.hsrw.dimitriosbarkas.ute.services.impl;
 
+import de.hsrw.dimitriosbarkas.ute.model.Task;
 import de.hsrw.dimitriosbarkas.ute.services.SafeExecuteTestService;
+import de.hsrw.dimitriosbarkas.ute.services.exceptions.CannotConvertToFileException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Base64;
 
+@Log4j2
+@Service
 public class SafeExecuteTestServiceImpl implements SafeExecuteTestService {
 
     @Override
-    public Path safelyExecuteTestInTempFolder(String encodedTask, String encodedTest) {
+    public Path extractFilesToTemplateProject(Task task, String encodedTest) throws CannotConvertToFileException {
+        byte[] testData = Base64.getDecoder().decode(encodedTest);
+        byte[] taskData = Base64.getDecoder().decode(task.getEncodedFile());
 
-        return null;
+        Process p;
+        try {
+            // Create temporary folder
+            Path path = Files.createTempDirectory("temp");
+
+            log.info("Extracting file content to path " + path.toAbsolutePath());
+
+            // Execute bash script
+            String[] command = {"bash", "src/main/resources/create-mvn-project-script.sh", "-p", path.toAbsolutePath().toString()};
+            p = Runtime.getRuntime().exec(command);
+
+            // Read output from script
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                log.info("Script output: " + line);
+            }
+
+            reader.close();
+
+            File taskFile = new File(path.toAbsolutePath() + "/testapp/src/main/java/com/test/app/" + task.getPathToFile());
+            File testFile = new File(path.toAbsolutePath() + "/testapp/src/test/java/com/test/app/" + task.getPathToTestTemplate());
+            writeFile(taskFile, taskData);
+            writeFile(testFile, testData);
+
+            //path.toFile().deleteOnExit();
+
+            return path;
+        } catch (IOException e) {
+            throw new CannotConvertToFileException(e);
+        }
+    }
+
+    @Override
+    public void safelyExecuteTestInTempProject(Path path) throws IOException {
+        String[] command = {"mvn", "test"};
+        String[] env = {};
+        String pathToTempProject = path.toAbsolutePath() + "/testapp";
+        File dir = new File(pathToTempProject);
+        Process p = Runtime.getRuntime().exec(command, env, dir);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            log.info("Script output: " + line);
+        }
+
+        log.info(dir);
+    }
+
+    @Override
+    public void generateCoverageReport(Path path) throws IOException{
+        String[] command = {"mvn", "jacoco:report"};
+        String[] env = {};
+        String pathToTempProject = path.toAbsolutePath() + "/testapp";
+        File dir = new File(pathToTempProject);
+        Process p = Runtime.getRuntime().exec(command, env, dir);
+    }
+
+    private void readCoverageReport(Path path) {
+        
+    }
+
+
+    private void writeFile(File file, byte[] data) {
+        try (FileOutputStream fos = new FileOutputStream(file, true)) {
+            String str = "package com.test.app; \n\n";
+            byte[] strToBytes = str.getBytes();
+            fos.write(strToBytes);
+            fos.write(data);
+            log.info(file.getAbsolutePath() + " saved.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
