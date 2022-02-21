3dafbaca782f0411399d6352dde11ae8d8181eb0
Dimitrios Barkas <53380520+dimibarkas@users.noreply.github.com>
2022-02-21 17:22:49 +0100

    refactoring CompilationErrorException

--- src/main/java/de/hsrw/dimitriosbarkas/ute/services/exceptions/CompilationErrorException.java
+++ src/main/java/de/hsrw/dimitriosbarkas/ute/services/exceptions/CompilationErrorException.java
@@ -3,14 +3,14 @@ package de.hsrw.dimitriosbarkas.ute.services.exceptions;
 /**
  * Exception that is thrown if tests could not be compiled.
  */
-public class CompliationErrorException extends Exception {
+public class CompilationErrorException extends Exception {
     /**
      * Default constructor.
      */
-    public CompliationErrorException() {
+    public CompilationErrorException() {
     }
 
-    public CompliationErrorException(Throwable cause) {
+    public CompilationErrorException(Throwable cause) {
         super(cause);
     }
 
