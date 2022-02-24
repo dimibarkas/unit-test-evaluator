4de0e48538eaf9dcbefd39e865c636b14af853e8
Dimitrios Barkas <53380520+dimibarkas@users.noreply.github.com>
2022-02-24 11:22:39 +0200

    add documentation to SafeExecuteTestService

--- src/main/java/de/hsrw/dimitriosbarkas/ute/services/SafeExecuteTestService.java
+++ src/main/java/de/hsrw/dimitriosbarkas/ute/services/SafeExecuteTestService.java
@@ -1,6 +1,7 @@
 package de.hsrw.dimitriosbarkas.ute.services;
 
 import de.hsrw.dimitriosbarkas.ute.model.Task;
+import de.hsrw.dimitriosbarkas.ute.model.TestResult;
 import de.hsrw.dimitriosbarkas.ute.model.jacocoreport.Report;
 import de.hsrw.dimitriosbarkas.ute.services.exceptions.CouldNotSetupTestEnvironmentException;
 import de.hsrw.dimitriosbarkas.ute.services.exceptions.ErrorWhileExecutingTestException;
@@ -10,11 +11,22 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public interface SafeExecuteTestService {
-
+    /**
+     * This function creates a maven project in a temporary directory where unit tests can be executed in.
+     * @param task the specified task which needs to be setup
+     * @param encodedTest the base 64 encoded test
+     * @return the path to the temporary directory
+     * @throws CouldNotSetupTestEnvironmentException if an error occurs while setting up the test environment
+     */
     Path setupTestEnvironment(Task task, String encodedTest) throws CouldNotSetupTestEnvironmentException;
 
-    //return the exit value of this process
-    void safelyExecuteTestInTempProject(Path path) throws ErrorWhileExecutingTestException, IOException;
+    /**
+     * This function actually calls the command to compile the provided tests.
+     * @param path the path to the temporary directory
+     * @return the exit value of the process
+     * @throws ErrorWhileExecutingTestException if an error occurs while executing tests (compile oder test errors)
+     */
+    TestResult executeTestInTempDirectory(Path path) throws ErrorWhileExecutingTestException, IOException, InterruptedException;
     
     //return the exit value of this process
     void generateCoverageReport(Path path) throws IOException, InterruptedException;
