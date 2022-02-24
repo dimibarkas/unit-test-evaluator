0ca643d1aba38c64da7562349256bb8c0935b181
Dimitrios Barkas <53380520+dimibarkas@users.noreply.github.com>
2022-02-24 11:24:01 +0200

    add documentation to EvaluatorService

--- src/main/java/de/hsrw/dimitriosbarkas/ute/services/EvaluatorService.java
+++ src/main/java/de/hsrw/dimitriosbarkas/ute/services/EvaluatorService.java
@@ -4,6 +4,16 @@ import de.hsrw.dimitriosbarkas.ute.services.exceptions.*;
 
 public interface EvaluatorService {
 
-    String evaluateTest(String taskId, String encodedTestContent) throws CannotLoadConfigException, TaskNotFoundException, CompilationErrorException;
+    /**
+     * This function takes the taskId and creates a testing environment to execute the encoded test file and returns an assumption.
+     * @param taskId the id for the specified test
+     * @param encodedTestContent the base 64 encoded test file
+     * @return assumption of execution as a string
+     * @throws CannotLoadConfigException if config file cannot be found
+     * @throws TaskNotFoundException if the specified task cannot be found
+     * @throws CompilationErrorException if an error occurred during the compilation
+     */
+    String evaluateTest(String taskId, String encodedTestContent)
+            throws CannotLoadConfigException, TaskNotFoundException, CompilationErrorException;
 
 }
