87cc224ed4d1f330465a4494dfb44fa155635380
Dimitrios Barkas <53380520+dimibarkas@users.noreply.github.com>
2022-02-25 13:22:57 +0100

    add documentation

--- src/main/java/de/hsrw/dimitriosbarkas/ute/services/SafeExecuteTestService.java
+++ src/main/java/de/hsrw/dimitriosbarkas/ute/services/SafeExecuteTestService.java
@@ -5,6 +5,7 @@ import de.hsrw.dimitriosbarkas.ute.model.TestResult;
 import de.hsrw.dimitriosbarkas.ute.model.jacocoreport.Report;
 import de.hsrw.dimitriosbarkas.ute.services.exceptions.CouldNotSetupTestEnvironmentException;
 import de.hsrw.dimitriosbarkas.ute.services.exceptions.ErrorWhileExecutingTestException;
+import de.hsrw.dimitriosbarkas.ute.services.exceptions.ErrorWhileGeneratingCoverageReport;
 import de.hsrw.dimitriosbarkas.ute.services.exceptions.JacocoReportXmlFileNotFoundException;
 
 import java.io.IOException;
@@ -15,7 +16,7 @@ public interface SafeExecuteTestService {
      * This function creates a maven project in a temporary directory where unit tests can be executed in.
      * @param task the specified task which needs to be setup
      * @param encodedTest the base 64 encoded test
-     * @return the path to the temporary directory
+     * @return path to the newly created temporary directory
      * @throws CouldNotSetupTestEnvironmentException if an error occurs while setting up the test environment
      */
     Path setupTestEnvironment(Task task, String encodedTest) throws CouldNotSetupTestEnvironmentException;
@@ -26,10 +27,14 @@ public interface SafeExecuteTestService {
      * @return the exit value of the process
      * @throws ErrorWhileExecutingTestException if an error occurs while executing tests (compile oder test errors)
      */
-    TestResult executeTestInTempDirectory(Path path) throws ErrorWhileExecutingTestException, IOException, InterruptedException;
-    
-    //return the exit value of this process
-    void generateCoverageReport(Path path) throws IOException, InterruptedException;
+    TestResult executeTestInTempDirectory(Path path) throws ErrorWhileExecutingTestException;
+
+    /**
+     * This function calls the command to generate a coverage-report.xml file out of the jacoco.exe file.
+     * @param path the path to the temporary maven directory
+     * @throws ErrorWhileGeneratingCoverageReport if an error occurs in time of the generation ( e.g. resource file could not be found)
+     */
+    void generateCoverageReport(Path path) throws ErrorWhileGeneratingCoverageReport;
 
     Report parseCoverageReport(Path path) throws IOException, JacocoReportXmlFileNotFoundException;
 
