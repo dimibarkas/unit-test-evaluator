b712b6408a5ab5270e1a8f9abc583e6219cb59f9
Dimitrios Barkas <53380520+dimibarkas@users.noreply.github.com>
2022-03-14 09:44:40 +0100

    add documentation to the task class

--- src/main/java/de/hsrw/dimitriosbarkas/ute/model/Task.java
+++ src/main/java/de/hsrw/dimitriosbarkas/ute/model/Task.java
@@ -8,19 +8,36 @@ import lombok.Data;
 @Data
 public class Task {
 
+    /**
+     * The id of the task.
+     */
     private String id;
-
+    /**
+     * The title of the task.
+     */
     private String name;
-
+    /**
+     * The description of the task.
+     */
     private String description;
-
+    /**
+     * The description of the target.
+     */
     private String targetDescription;
-
+    /**
+     * The path to the file.
+     */
     private String pathToFile;
-
+    /**
+     * The file content base-64 encoded.
+     */
     private String encodedFile;
-
+    /**
+     * The path to the file with the test-template.
+     */
     private String pathToTestTemplate;
-
+    /**
+     * The test template file content base-64 encoded.
+     */
     private String encodedTestTemplate;
 }
