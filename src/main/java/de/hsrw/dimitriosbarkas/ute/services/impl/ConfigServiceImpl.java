94fca37222dc5e198ffb5a064a0759f77d9fccfb
Dimitrios Barkas <53380520+dimibarkas@users.noreply.github.com>
2022-06-03 11:57:29 +0200

    bugfix could not load file content from resource

--- src/main/java/de/hsrw/dimitriosbarkas/ute/services/impl/ConfigServiceImpl.java
+++ src/main/java/de/hsrw/dimitriosbarkas/ute/services/impl/ConfigServiceImpl.java
@@ -5,25 +5,35 @@ import de.hsrw.dimitriosbarkas.ute.model.TaskConfig;
 import de.hsrw.dimitriosbarkas.ute.services.ConfigService;
 import de.hsrw.dimitriosbarkas.ute.services.exceptions.CannotLoadConfigException;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Base64;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 
 @Service
 @Log4j2
 public class ConfigServiceImpl implements ConfigService {
 
-    public String loadResourceByUrl(URL u) throws IOException {
-        if (u == null) return null;
-        return new String(Files.readAllBytes(Paths.get(u.getPath())));
+    public static String readFileToString(String path) {
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource(path);
+        return asString(resource);
+    }
+
+    public static String asString(Resource resource) {
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     @Override
@@ -33,7 +43,7 @@ public class ConfigServiceImpl implements ConfigService {
 
         URL url = getClass().getClassLoader().getResource(TASKS_DEFINITION_FILE);
         if (url == null) throw new CannotLoadConfigException();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), UTF_8))) {
             YamlReader reader = new YamlReader(br);
 
             TaskConfig taskConfig = reader.read(TaskConfig.class);
@@ -41,21 +51,15 @@ public class ConfigServiceImpl implements ConfigService {
             taskConfig.getTasks().forEach(task -> {
                 String pathToFile = task.getPathToDir() + task.getSourcefilename();
                 String pathToTemplate = task.getPathToDir() + task.getTesttemplatefilename();
-                URL fileURL = getClass().getClassLoader().getResource(pathToFile);
-                URL templateURL = getClass().getClassLoader().getResource(pathToTemplate);
-                try {
-                    String fileContent = loadResourceByUrl(fileURL);
-                    if (fileContent != null) {
-                        String encodedFile = Base64.getEncoder().encodeToString(fileContent.getBytes(StandardCharsets.UTF_8));
-                        task.setEncodedFile(encodedFile);
-                    }
-                    String templateContent = loadResourceByUrl(templateURL);
-                    if (templateContent != null) {
-                        String encodedTemplate = Base64.getEncoder().encodeToString(templateContent.getBytes(StandardCharsets.UTF_8));
-                        task.setEncodedTestTemplate(encodedTemplate);
-                    }
-                } catch (IOException e) {
-                    log.error(e);
+                String fileContent = readFileToString(pathToFile);
+                if (!fileContent.isEmpty()) {
+                    String encodedFile = Base64.getEncoder().encodeToString(fileContent.getBytes(UTF_8));
+                    task.setEncodedFile(encodedFile);
+                }
+                String templateContent = readFileToString(pathToTemplate);
+                if (!templateContent.isEmpty()) {
+                    String encodedTemplate = Base64.getEncoder().encodeToString(templateContent.getBytes(UTF_8));
+                    task.setEncodedTestTemplate(encodedTemplate);
                 }
             });
 
